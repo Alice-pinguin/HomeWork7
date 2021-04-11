7fd13e5a1d0b0c6f06b5eb60b9dcd18bd6528d48
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-04-11 16:00:05 +0300

    Добавлен класс Rhombus

--- Rhombus.java
+++ Rhombus.java
@@ -1,20 +1,17 @@
 package company;
-
-class Rhombus extends Shape implements ColorShape{
-    private String name ;
-    private String color;
-
-    public Rhombus(String name, String color) {
+class Rhombus extends Shape {
+    private String name, color;
+    public Rhombus (String name, String color) {
         this.name = name;
         this.color = color;
     }
-
     @Override
     public String getName() {
         return name;
     }
+
     @Override
     public String getColor() {
-          return color;
+        return color;
     }
 }
