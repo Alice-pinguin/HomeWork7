54cffbec3c9d6ca0cb90ea2c0e5413f36b9ce827
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-04-11 15:59:31 +0300

    Добавлен класс  Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,20 +1,18 @@
 package company;
 
 class Rectangle extends Shape {
-    private String name ;
-    private String color;
-
+    private String name, color;
+    public Rectangle (String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
     @Override
     public String getName() {
         return name;
     }
+
     @Override
     public String getColor() {
         return color;
-    }
-
-    public Rectangle(String name, String color) {
-        this.name = name;
-        this.color = color;
     }
 }
