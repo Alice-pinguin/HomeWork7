ed023a7198881e6266dcac407e0186e6d854aa73
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-04-13 07:58:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import company.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle("Circle", "grey");
+        Quad quad = new Quad("Quad", "green");
+        Rhombus rhombus = new Rhombus("Rhombus", "blue");
+        Parallelogram parallelogram = new Parallelogram("Parallelogram", "yellow");
+        Rectangle rectangle = new Rectangle("Rectangle", "black");
+        NamePrinter namePrinter = new NamePrinter();
+        namePrinter.printColor(circle);
+        namePrinter.printName(circle);
+        namePrinter.printColor(quad);
+        namePrinter.printName(quad);
+        namePrinter.printColor(rhombus);
+        namePrinter.printName(rhombus);
+        namePrinter.printColor(parallelogram);
+        namePrinter.printName(parallelogram);
+        namePrinter.printColor(rectangle);
+        namePrinter.printName(rectangle);
+    }
+}
+
+
