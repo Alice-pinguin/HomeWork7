d66b0d20b41a92a6c03ff262e79d3707280fd19b
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-04-13 07:56:34 +0300

    Update NamePrinter.java

--- NamePrinter.java
+++ NamePrinter.java
@@ -8,27 +8,6 @@ public class NamePrinter {
    public void printColor(Shape shape) {
      System.out.println(shape.getColor());
    }
-
-   public static void main(String[] args) {
-
-     Circle circle = new Circle("Circle", "grey");
-     Quad quad = new Quad("Quad", "green");
-     Rhombus rhombus = new Rhombus("Rhombus", "blue");
-     Parallelogram parallelogram = new Parallelogram("Parallelogram", "yellow");
-     Rectangle rectangle = new Rectangle("Rectangle", "black");
-     NamePrinter namePrinter = new NamePrinter();
-     namePrinter.printColor(circle);
-     namePrinter.printName(circle);
-     namePrinter.printColor(quad);
-     namePrinter.printName(quad);
-     namePrinter.printColor(rhombus);
-     namePrinter.printName(rhombus);
-     namePrinter.printColor(parallelogram);
-     namePrinter.printName(parallelogram);
-     namePrinter.printColor(rectangle);
-     namePrinter.printName(rectangle);
-   }
- }
-
+}
 
 
